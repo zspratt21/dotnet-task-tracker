fb98c333f7efbed74e81d0a0500ebd96934f3882
Zak <zspratt21@gmail.com>
2023-10-11 23:08:53 +0800

    Setup base application and env file processing.

---
+++ .bashrc
@@ -0,0 +1,3 @@
+﻿alias dotnet-sdk='docker-compose exec dotnet_sdk'
+alias dotnet-run='docker-compose exec dotnet_sdk dotnet watch run'
+alias dotnet-nuget='docker-compose exec dotnet_sdk dotnet nuget'
---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using DotEnv.Core;
+
+new EnvLoader()
+    .AddEnvFile("../env/.env")
+    .Load();
+
+var env = new EnvReader();
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello world!!!");
+
+app.Run("http://"+env["HOST"]+":"+env["PORT"]+"");
